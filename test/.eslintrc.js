6f147d5eb922d76414cfb1472aa8b30bca69039b
Restyled.io <commits@restyled.io>
2019-11-20 08:02:39 +0000

    Restyled by prettier

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  env : {
-    jest : true,
+  env: {
+    jest: true,
   },
-  rules : {
-    'prefer-destructuring' : 0,
-    'prefer-promise-reject-errors' : 0,
-    'import/no-dynamic-require' : 0,
-    'import/no-extraneous-dependencies' : 0,
-    'import/no-named-as-default-member' : 0,
-    'global-require' : 0,
+  rules: {
+    'prefer-destructuring': 0,
+    'prefer-promise-reject-errors': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default-member': 0,
+    'global-require': 0,
 
-    '@typescript-eslint/no-var-requires' : 0,
-    '@typescript-eslint/no-object-literal-type-assertion' : 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-object-literal-type-assertion': 0,
   },
 };
